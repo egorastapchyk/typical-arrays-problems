55495944daeb66b023bf5b3be729405dec3af30f
egorastapchyk <41088004+egorastapchyk@users.noreply.github.com>
2021-03-10 18:31:49 +0300

    feat: add function to index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    return array.sort((a, b) => {
+        return a - b;
+    })[0];
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    return array
+        .sort((a, b) => {
+            return a - b;
+        })
+        .reverse()[0];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let num = 0;
+    for (let i = 0; i < array.length; i++) {
+        num += array[i];
+    }
+    return num / array.length;
+};
